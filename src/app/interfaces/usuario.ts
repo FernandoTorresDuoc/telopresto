0dfee75b331247934c8b1b0cf2eb5f3814d9b2ad
felipencio94 <feli.fuentealba@duocuc.cl>
2022-05-15 21:35:53 -0400

    commit 1

---
+++ src/app/interfaces/usuario.ts
@@ -0,0 +1,19 @@
+
+
+export interface Usuario {
+    id_usuario?:number;
+    run?: number;
+    dv?:string;
+    pri_nom?:string;
+    seg_nom?:string;
+    pri_ap?:string;
+    seg_ap?:string;
+    direccion?:string;
+    id_comuna?:number;
+    mail?:string;
+    password?:string;
+    sexo?:string;
+    rol?:string;
+    estado?:string;
+
+}
